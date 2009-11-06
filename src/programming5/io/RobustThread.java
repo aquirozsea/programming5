be2fb8e8cccccacf830454e3c16d277df0b62beb
aquirozh@gmail.com <aquirozh@gmail.com@9f01d7b8-49a5-11de-a4f5-9d9bc3506571>
2009-11-06 17:17:29 +0000

    Added ExceptionHandler and documented RobustThread

--- src/programming5/io/RobustThread.java
+++ src/programming5/io/RobustThread.java
@@ -22,53 +22,143 @@
 package programming5.io;
 
 /**
- *
- * @author aquirozh
+ * A robust thread is a thread that catches all uncaught exceptions from its runnable target and, by
+ * default, restarts the target to avoid fatal crashes. Although the thread terminates if its target
+ * run method exits without exceptions, this class is meant for long running or daemon threads. An
+ * exception handler interface is provided in order to do exception handling at thread level and/or
+ * perform necessary cleanup before either restarting or terminating the thread.
+ * <p> The RobustThread run method is declared final, so a subclass of RobustThread cannot itself be
+ * used as a runnable object, as with the Thread base class. Instead, when started, an instance of this
+ * class always runs an implementation of the Runnable interface as a target. This is because the run
+ * method of the class implements the exception handling and restart functionality.
+ * @author Andres Quiroz Hernandez
+ * @version 6.0
  */
-public final class RobustThread extends Thread {
+public class RobustThread extends Thread {
 
-    boolean running;
-    Runnable target;
+    protected Runnable target;
+    protected ExceptionHandler exceptionHandler = null;
+    private boolean running;
 
+    /**
+     * @param target the class that implements the run method that will be called when this thread is 
+     * started
+     */
     public RobustThread(Runnable target) {
         super();
         this.target = target;
     }
 
+    /**
+     * @param target the class that implements the run method that will be called when this thread is 
+     * started
+     * @param name the name of the thread
+     */
     public RobustThread(Runnable target, String name) {
         super(name);
         this.target = target;
     }
 
+    /**
+     * @param group the thread's group, as defined for the Thread class
+     * @param target the class that implements the run method that will be called when this thread is 
+     * started
+     */
     public RobustThread(ThreadGroup group, Runnable target) {
         super(group, "");
         this.target = target;
     }
 
+    /**
+     * @param group the thread's group, as defined for the Thread class
+     * @param target the class that implements the run method that will be called when this thread is
+     * started
+     * @param name the name of the thread
+     */
     public RobustThread(ThreadGroup group, Runnable target, String name) {
         super(group, name);
         this.target = target;
     }
 
+    /**
+     * @param target the class that implements the run method that will be called when this thread is 
+     * started
+     * @param myExceptionHandler the class that implements the handleException method that will be 
+     * called if the given target throws an uncaught exception
+     */
+    public RobustThread(Runnable target, ExceptionHandler myExceptionHandler) {
+        super();
+        this.target = target;
+        exceptionHandler = myExceptionHandler;
+    }
+
+    /**
+     * @param target the class that implements the run method that will be called when this thread is
+     * started
+     * @param name the name of the thread
+     * @param myExceptionHandler the class that implements the handleException method that will be
+     * called if the given target throws an uncaught exception
+     */
+    public RobustThread(Runnable target, String name, ExceptionHandler myExceptionHandler) {
+        super(name);
+        this.target = target;
+        exceptionHandler = myExceptionHandler;
+    }
+
+    /**
+     * @param group the thread's group, as defined for the Thread class
+     * @param target the class that implements the run method that will be called when this thread is
+     * started
+     * @param myExceptionHandler the class that implements the handleException method that will be
+     * called if the given target throws an uncaught exception
+     */
+    public RobustThread(ThreadGroup group, Runnable target, ExceptionHandler myExceptionHandler) {
+        super(group, "");
+        this.target = target;
+        exceptionHandler = myExceptionHandler;
+    }
+
+    /**
+     * @param group the thread's group, as defined for the Thread class
+     * @param target the class that implements the run method that will be called when this thread is
+     * started
+     * @param name the name of the thread
+     * @param myExceptionHandler the class that implements the handleException method that will be
+     * called if the given target throws an uncaught exception
+     */
+    public RobustThread(ThreadGroup group, Runnable target, String name, ExceptionHandler myExceptionHandler) {
+        super(group, name);
+        this.target = target;
+        exceptionHandler = myExceptionHandler;
+    }
+
+    /**
+     * Called when the thread is started, an in turn calls the target's run method
+     */
     @Override
-    public void run() {
+    public final void run() {
         running = true;
         while (running) {
             try {
                 target.run();
-                System.out.println("Thread finished");
                 running = false;
             }
             catch (Exception e) {
-                Debug.println("Exception in thread " + this.getName() + ": " + e.toString(), "programming5.io.RobustThread");
-                Debug.println("RobustThread: Restarting thread " + this.getName(), "programming5.io.RobustThread");
+                if (exceptionHandler != null) {
+                    ExceptionHandler.ThreadAction action = exceptionHandler.handleException(e);
+                    if (action.equals(ExceptionHandler.ThreadAction.TERMINATE)) {
+                        running = false;
+                    }
+                    else {
+                        Debug.println("RobustThread: Restarting thread " + this.getName(), "programming5.io.RobustThread");
+                    }
+                }
+                else {
+                    Debug.println("Exception in thread " + this.getName() + ": " + e.toString(), "programming5.io.RobustThread");
+                    Debug.println("RobustThread: Restarting thread " + this.getName(), "programming5.io.RobustThread");
+                }
             }
         }
-    }
-
-    public void terminate() {
-        running = false;
-        this.stop();
     }
 
 }
