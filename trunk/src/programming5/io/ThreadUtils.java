6d84c1744684628dff6adf23e05c99c1a5293f0e
aquirozh <aquirozh@9f01d7b8-49a5-11de-a4f5-9d9bc3506571>
2010-11-27 20:31:52 +0000

    Added documentation and new sleep method to the ThreadUtils class

--- trunk/src/programming5/io/ThreadUtils.java
+++ trunk/src/programming5/io/ThreadUtils.java
@@ -6,16 +6,35 @@
 package programming5.io;
 
 /**
- *
+ * Abstract utility class for threads
  * @author aquirozh
  */
 public abstract class ThreadUtils {
 
+    /**
+     * Wrapper method for the Thread.sleep method, to avoid code clutter from the Interrupted exception, which 
+     * will be ignored.
+     * @param timeMillis the length of time for the thread to sleep
+     */
     public static void sleep(long timeMillis) {
         try {
             Thread.sleep(timeMillis);
         }
         catch (InterruptedException ie) {}
+    }
+
+    /**
+     * Wrapper method for the Thread.sleep method, to avoid code clutter from the Interrupted exception, which
+     * will be handled by the given exception handler.
+     * @param timeMillis the length of time for the thread to sleep
+     */
+    public static void sleep(long timeMillis, ExceptionHandler exceptionHandler) {
+        try {
+            Thread.sleep(timeMillis);
+        }
+        catch (InterruptedException ie) {
+            exceptionHandler.handleException(ie);
+        }
     }
 
 }
