ae2610293fd96cafb0d7927831d0b165e2d9837f
aquirozh@gmail.com <aquirozh@gmail.com@9f01d7b8-49a5-11de-a4f5-9d9bc3506571>
2012-07-26 19:27:28 +0000

    Added DBType enumeration and related constructors and methods to JdbcDriver class to facilitate instantiation with known connection types and drivers.

--- trunk/src/programming5/io/jdbc/JdbcDriver.java
+++ trunk/src/programming5/io/jdbc/JdbcDriver.java
@@ -30,36 +30,129 @@ import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import programming5.arrays.ArrayOperations;
 import programming5.io.Debug;
+import programming5.strings.KeyValuePairMatcher;
 
 /**
  * This class is a utility wrapper for a JDBC database connection, providing basic CRUD methods
- * @version 6.19
+ * @version 6.2
  */
 public class JdbcDriver {
+
+    public enum DBType {
+
+        mysql ("jdbc:mysql", "com.mysql.jdbc.Driver", 3306),
+        postgresql ("jdbc:postgresql", "org.postgresql.Driver", 5432);
+
+        private final String connectSchema;
+        private final String myDriver;
+        private final int defaultPort;
+
+        DBType(String refSchema, String refDriver, int refPort) {
+            connectSchema = refSchema;
+            myDriver = refDriver;
+            defaultPort = refPort;
+        }
+
+        public String getConnectSchema() {
+            return connectSchema;
+        }
+
+        public String getDriver() {
+            return myDriver;
+        }
+
+        public int getDefaultPort() {
+            return defaultPort;
+        }
+
+        public String getDefaultPortString() {
+            return Integer.toString(defaultPort);
+        }
+
+    };
+
+    public static enum DBPropertyKey {user, password, host, port, driver};
 
     Connection db = null;
     Map<String, Savepoint> savepoints = new HashMap<String, Savepoint>();
 
-//    static {
-//        try {
-//            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-//        }
-//        catch (ClassNotFoundException cnfe) {
-//            cnfe.printStackTrace();
-//        }
-//    }
-
+    /**
+     * Starts a driver instance making a connection to the specified database. It is assumed that no username/password are required for 
+     * the connection.
+     * This constructor requires the DB driver to be registered externally beforehand
+     * @param dbURL a URL of the form "<connect_schema>://<host>:<port>/<dbname>"
+     */
     public JdbcDriver(String dbURL) throws SQLException {
         db = DriverManager.getConnection(dbURL);
     }
 
-    public JdbcDriver(String dbURL, Properties connectProperties) throws SQLException {
-        db = DriverManager.getConnection(dbURL, connectProperties);
-    }
-    
+    /**
+     * Starts a driver instance making a connection to the specified database, with the username/password provided
+     * This constructor requires the DB driver to be registered externally beforehand
+     * @param dbURL a URL of the form "<connect_schema>://<host>:<port>/<dbname>"
+     */
     public JdbcDriver(String dbURL, String user, String password) throws SQLException {
         db = DriverManager.getConnection(dbURL, user, password);
+    }
+
+    /**
+     * Starts a driver instance making a connection to the specified database. Username/password required for 
+     * the connection and other properties may be specified in the connect properties parameter, using keys in DBPropertyKey enumeration 
+     * provided by the class.
+     * The DB driver must either be registered externally beforehand, or a driver class name must be provided in the properties object
+     * using DBPropertyKey.driver
+     * @param dbURL a URL of the form "<connect_schema>://<host>:<port>/<dbname>"
+     * @param connectProperties properties object with key strings given by DBPropertyKey enumeration 
+     */
+    public JdbcDriver(String dbURL, Properties connectProperties) throws SQLException {
+        String driverClass = connectProperties.getProperty(DBPropertyKey.driver.toString());
+        if (driverClass != null) {
+            try {
+                Class.forName(driverClass);
+            }
+            catch (Exception e){
+                throw new SQLException("JdbcDriver: Could not register database driver: " + e.getMessage(), e);
+            }
+        }
+        db = DriverManager.getConnection(dbURL, connectProperties);
+    }
+
+    /**
+     * Simplified constructor that encapsulates commonly used drivers for particular database types. Uses an inner enumeration of database 
+     * types, and the known connect schema and drivers for each. The constructor will register the driver and create the connection.
+     * Username/password required for the connection and other properties may be specified in the connect properties parameter, using 
+     * keys in DBPropertyKey enumeration provided by the class. 
+     * @param dbType type from the inner enumeration DBType
+     * @param databaseName database name
+     * @param connectProperties zero or more properties that may be required for creating the connection, given as key:value pairs (e.g. "user:admin")
+     */
+    public JdbcDriver(DBType dbType, String databaseName, String... connectProperties) throws SQLException {
+        try {
+            Class.forName(dbType.getDriver());
+        }
+        catch (Exception e){
+            throw new SQLException("JdbcDriver: Could not register database driver: " + e.getMessage(), e);
+        }
+        String host = fillParameter(DBPropertyKey.host, connectProperties, "localhost");
+        String port = fillParameter(DBPropertyKey.port, connectProperties, dbType.getDefaultPortString());
+        // Create connect URL with given or default values
+        String dbURL = dbType.getConnectSchema() + "://" + host + ":" + port + "/" + databaseName;
+        // Fill user and password, if given
+        String user = fillParameter(DBPropertyKey.user, connectProperties, null);
+        String password = fillParameter(DBPropertyKey.password, connectProperties, null);
+        if (user != null) {
+            Properties propertiesObject = new Properties();
+            propertiesObject.put(DBPropertyKey.user.toString(), user);
+            if (password != null) {
+                propertiesObject.put(DBPropertyKey.password.toString(), password);
+            }
+            db = DriverManager.getConnection(dbURL, propertiesObject);
+        }
+        else {
+            db = DriverManager.getConnection(dbURL);
+        }
     }
 
     public void createTable(String name, String[] fields, String[] types) throws SQLException {
@@ -244,6 +337,18 @@ public class JdbcDriver {
             throw new IllegalArgumentException("Null list");
         }
         return line;
+    }
+
+    private String fillParameter(DBPropertyKey key, String[] properties, String defaultValue) {
+        String ret;
+        int pos = ArrayOperations.seqFind(key.toString(), properties, new KeyValuePairMatcher());
+        if (pos >= 0) {
+            ret = properties[pos].split(":")[1].trim();
+        }
+        else {
+            ret = defaultValue;
+        }
+        return ret;
     }
 
 }
