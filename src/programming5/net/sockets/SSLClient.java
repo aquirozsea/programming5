6816ed31aa3921df2260be8fc397098c26937afc
aquirozh@gmail.com <aquirozh@gmail.com@9f01d7b8-49a5-11de-a4f5-9d9bc3506571>
2012-10-12 20:55:15 +0000

    Created new classes for use of SSL (secure sockets)

---
+++ src/programming5/net/sockets/SSLClient.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package programming5.net.sockets;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import javax.net.ssl.SSLSocketFactory;
+import programming5.net.NetworkException;
+
+/**
+ *
+ * @author andresqh
+ */
+public class SSLClient extends TCPClient {
+
+    public SSLClient() throws IOException {
+        super(SSLSocketFactory.getDefault().createSocket());
+    }
+
+    /**
+     *Creates an SSL client for the specified host address at the specified port. Will bind the client to any available local port.
+     */
+    public SSLClient(String address, int remotePort) throws IOException {
+        super(SSLSocketFactory.getDefault().createSocket(address, remotePort));
+    }
+
+    /**
+     *Creates an SSL client on the given local port for the specified host address at the specified port.
+     */
+    public SSLClient(String address, int remotePort, int localPort) throws IOException {
+        super(SSLSocketFactory.getDefault().createSocket(address, remotePort, InetAddress.getLocalHost(), localPort));
+    }
+
+    /**
+     *Implementation of the PluggableClient interface. Opens a socket to each of the host
+     *addresses with which the client has been created, gets their output streams, and starts their receiver
+     *threads.
+     */
+    @Override
+    public void establishConnection() throws NetworkException {
+        // No need for implementation until pending connections is used
+    }
+
+}
