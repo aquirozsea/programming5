72eca7d500b607ff6a7f42e6b2154647a881e6e4
aquirozh@gmail.com <aquirozh@gmail.com@9f01d7b8-49a5-11de-a4f5-9d9bc3506571>
2014-01-03 05:16:53 +0000

    Added arbitrary strings to make testing of distance functions more robust

--- test/programming5/strings/AlphabeticDistanceFunctionTest.java
+++ test/programming5/strings/AlphabeticDistanceFunctionTest.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.*;
 public class AlphabeticDistanceFunctionTest {
 
     static AlphabeticDistanceFunction adf;
+    static String arbitrary1, arbitrary2, arbitrary3;
 
     public AlphabeticDistanceFunctionTest() {
     }
@@ -26,6 +27,10 @@ public class AlphabeticDistanceFunctionTest {
     @BeforeClass
     public static void setUpClass() throws Exception {
         adf = new AlphabeticDistanceFunction();
+        RandomStringGenerator rsg = new RandomStringGenerator();
+        arbitrary1 = rsg.generateAlphabeticMixedCaseString();
+        arbitrary2 = rsg.generateAlphabeticMixedCaseString();
+        arbitrary3 = rsg.generateAlphabeticMixedCaseString();
     }
 
     @AfterClass
@@ -62,9 +67,11 @@ public class AlphabeticDistanceFunctionTest {
     @Test
     public void testEquals() {
         assertEquals(0, (int) adf.distance("equal", "equal"));
-        assertEquals(0, (int) adf.distance("also equal", "also equal"));
         assertEquals(0, (int) adf.distance("", ""));
         assertEquals(0, (int) adf.distance("capsinsensitive", "CAPSINSENSITIVE"));
+        assertEquals(0, adf.distance(arbitrary1, arbitrary1), 1e-15);
+        assertEquals(0, adf.distance(arbitrary2, arbitrary2), 1e-15);
+        assertEquals(0, adf.distance(arbitrary3, arbitrary3), 1e-15);
     }
 
     /**
@@ -75,8 +82,9 @@ public class AlphabeticDistanceFunctionTest {
         assertTrue("Distance must always be non-negative", adf.distance("s1", "s2") >= 0);
         assertTrue("Distance must always be non-negative", adf.distance("", "s2") >= 0);
         assertTrue("Distance must always be non-negative", adf.distance("other", "") >= 0);
-        assertTrue("Distance must always be non-negative", adf.distance("longer strings", "adding to length") >= 0);
-        assertTrue("Distance must always be non-negative", adf.distance("adding to length", "longer strings") >= 0);
+        assertTrue("Distance must always be non-negative", adf.distance(arbitrary1, arbitrary2) >= 0);
+        assertTrue("Distance must always be non-negative", adf.distance(arbitrary2, arbitrary3) >= 0);
+        assertTrue("Distance must always be non-negative", adf.distance(arbitrary3, arbitrary1) >= 0);
     }
 
     /**
@@ -86,7 +94,9 @@ public class AlphabeticDistanceFunctionTest {
     public void testCommutative() {
         assertEquals(adf.distance("s1", "s2"), adf.distance("s2", "s1"), 0);
         assertEquals(adf.distance("s1", ""), adf.distance("", "s1"), 0);
-        assertEquals(adf.distance("longer strings", "adding to length"), adf.distance("adding to length", "longer strings"), 0);
+        assertEquals(adf.distance(arbitrary1, arbitrary2), adf.distance(arbitrary2, arbitrary1), 0);
+        assertEquals(adf.distance(arbitrary2, arbitrary3), adf.distance(arbitrary3, arbitrary2), 0);
+        assertEquals(adf.distance(arbitrary1, arbitrary3), adf.distance(arbitrary3, arbitrary1), 0);
     }
 
     /**
@@ -96,7 +106,7 @@ public class AlphabeticDistanceFunctionTest {
     public void testTriangle() {
         assertTrue("Distance must obey the triangle inequality", adf.distance("sa", "sb") + adf.distance("sb", "sc") >= adf.distance("sa", "sc"));
         assertTrue("Distance must obey the triangle inequality", adf.distance("sa", "") + adf.distance("", "sc") >= adf.distance("sa", "sc"));
-        assertTrue("Distance must obey the triangle inequality", adf.distance("arbitrary", "other") + adf.distance("other", "sobering") >= adf.distance("arbitrary", "sobering"));
+        assertTrue("Distance must obey the triangle inequality (Failed with " + arbitrary1 + ", " + arbitrary2 + ", " + arbitrary3 + ")", adf.distance(arbitrary1, arbitrary2) + adf.distance(arbitrary2, arbitrary3) >= adf.distance(arbitrary1, arbitrary3));
     }
 
 }
--- test/programming5/strings/KeyboardCharDistanceFunctionTest.java
+++ test/programming5/strings/KeyboardCharDistanceFunctionTest.java
@@ -5,6 +5,7 @@
 
 package programming5.strings;
 
+import programming5.code.RandomIndexGenerator;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,6 +21,7 @@ import static org.junit.Assert.*;
 public class KeyboardCharDistanceFunctionTest {
 
     static DistanceFunction<Character> df;
+    static char arbitrary1, arbitrary2, arbitrary3;
 
     public KeyboardCharDistanceFunctionTest() {
     }
@@ -27,6 +29,10 @@ public class KeyboardCharDistanceFunctionTest {
     @BeforeClass
     public static void setUpClass() throws Exception {
         df = new KeyboardCharDistanceFunction();
+        RandomIndexGenerator rig = new RandomIndexGenerator("32-126");
+        arbitrary1 = (char) rig.getRandomIndex();
+        arbitrary2 = (char) rig.getRandomIndex();
+        arbitrary3 = (char) rig.getRandomIndex();
     }
 
     @AfterClass
@@ -64,9 +70,12 @@ public class KeyboardCharDistanceFunctionTest {
      */
     @Test
     public void testEquals() {
-        assertEquals(0, df.distance('e', 'e'), 1e-5);
-        assertEquals(0, df.distance(' ', ' '), 1e-5);
+        assertEquals(0, df.distance('e', 'e'), 1e-15);
+        assertEquals(0, df.distance(' ', ' '), 1e-15);
         assertFalse(df.distance('a', 'A') == 0);
+        assertEquals(0, df.distance(arbitrary1, arbitrary1), 1e-15);
+        assertEquals(0, df.distance(arbitrary2, arbitrary2), 1e-15);
+        assertEquals(0, df.distance(arbitrary3, arbitrary3), 1e-15);
     }
 
     /**
@@ -77,6 +86,9 @@ public class KeyboardCharDistanceFunctionTest {
         assertTrue("Distance must always be non-negative", df.distance('s', 's') >= 0);
         assertTrue("Distance must always be non-negative", df.distance('o', '*') >= 0);
         assertTrue("Distance must always be non-negative", df.distance('B', '9') >= 0);
+        assertTrue("Distance must always be non-negative", df.distance(arbitrary1, arbitrary2) >= 0);
+        assertTrue("Distance must always be non-negative", df.distance(arbitrary2, arbitrary3) >= 0);
+        assertTrue("Distance must always be non-negative", df.distance(arbitrary3, arbitrary1) >= 0);
     }
 
     /**
@@ -85,6 +97,9 @@ public class KeyboardCharDistanceFunctionTest {
     @Test
     public void testCommutative() {
         assertEquals(df.distance('1', '2'), df.distance('2', '1'), 0);
+        assertEquals(df.distance(arbitrary1, arbitrary2), df.distance(arbitrary2, arbitrary1), 0);
+        assertEquals(df.distance(arbitrary2, arbitrary3), df.distance(arbitrary3, arbitrary2), 0);
+        assertEquals(df.distance(arbitrary1, arbitrary3), df.distance(arbitrary3, arbitrary1), 0);
     }
 
     /**
@@ -93,6 +108,7 @@ public class KeyboardCharDistanceFunctionTest {
     @Test
     public void testTriangle() {
         assertTrue("Distance must obey the triangle inequality", df.distance('a', 'b') + df.distance('b', 'c') >= df.distance('a', 'c'));
+        assertTrue("Distance must obey the triangle inequality", df.distance(arbitrary1, arbitrary2) + df.distance(arbitrary2, arbitrary3) >= df.distance(arbitrary1, arbitrary3));
     }
 
 }
