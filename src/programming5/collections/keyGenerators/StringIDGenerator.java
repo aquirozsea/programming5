b5da7ca35057ac726edc9c165cedaec42fd52295
aquirozh@gmail.com <aquirozh@gmail.com@9f01d7b8-49a5-11de-a4f5-9d9bc3506571>
2014-02-10 23:17:53 +0000

    Added new key generator to collections package for generating id strings

---
+++ src/programming5/collections/keyGenerators/StringIDGenerator.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package programming5.collections.keyGenerators;
+
+import java.util.Random;
+import programming5.collections.MapKeyGenerator;
+
+/**
+ *
+ * @author andresqh
+ */
+public class StringIDGenerator implements MapKeyGenerator {
+
+    protected Random random;
+    protected int length;
+
+    public StringIDGenerator() {
+        random = new Random(System.currentTimeMillis());
+        length = 8;
+    }
+
+    public StringIDGenerator(long randomSeed) {
+        random = new Random(randomSeed);
+        length = 8;
+    }
+
+    public StringIDGenerator(int idLength) {
+        random = new Random(System.currentTimeMillis());
+        length = idLength;
+    }
+    
+    public StringIDGenerator(int idLength, long randomSeed) {
+        random = new Random(randomSeed);
+        length = idLength;
+    }
+    
+    public Object generateKey() {
+        char[] stringBytes = new char[length];
+        for (int i = 0; i < stringBytes.length; i++) {
+            stringBytes[i] = (char) (random.nextInt(26) + 97);
+        }
+        return new String(stringBytes);
+    }
+}
