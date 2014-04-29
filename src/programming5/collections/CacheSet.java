6d45230396fde708752f79556d37e3b1b9aa53b9
aquirozh@gmail.com <aquirozh@gmail.com@9f01d7b8-49a5-11de-a4f5-9d9bc3506571>
2014-04-29 20:28:28 +0000

    Tested cache set

--- src/programming5/collections/CacheSet.java
+++ src/programming5/collections/CacheSet.java
@@ -5,7 +5,10 @@
 
 package programming5.collections;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Random;
+import programming5.arrays.RandomOrderGenerator;
 
 /**
  * Hash set that keeps the last inserted of equal versions. Useful when items have extra data not used
@@ -13,6 +16,31 @@ import java.util.HashSet;
  * @author andresqh
  */
 public class CacheSet<T> extends HashSet<T> {
+    
+    public CacheSet() {
+        super();
+    }
+
+    /**
+     *Creates a new set out of the collection elements. Any repeated elements in the collection will only be inserted once.
+     */
+    public CacheSet(Collection<? extends T> c) {
+        super(c);
+    }
+
+    /**
+     *Creates an empty set initialized to the given size
+     */
+    public CacheSet(int size) {
+        super(size);
+    }
+
+    /**
+     *Creates an empty set initialized to the given size with the given growth percentage
+     */
+    public CacheSet(int size, float pctGrowth) {
+        super(size, pctGrowth);
+    }
 
     @Override
     public boolean add(T e) {
