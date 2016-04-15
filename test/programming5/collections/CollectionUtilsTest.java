2ecb87b4f5003cc9a0ef96c1262fb8d90e63cc38
aquirozsea <aquiroz.sea@gmail.com>
2016-04-14 22:40:43 -0400

    Testing list synchronization method

--- test/programming5/collections/CollectionUtilsTest.java
+++ test/programming5/collections/CollectionUtilsTest.java
@@ -2,10 +2,11 @@ package programming5.collections;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class CollectionUtilsTest {
 
@@ -17,6 +18,22 @@ public class CollectionUtilsTest {
         assertEquals("one", CollectionUtils.toPrintableList(singleList, "???"));
         assertEquals("one, two, three", CollectionUtils.toPrintableList(multiList));
         assertEquals("one::two::three", CollectionUtils.toPrintableList(multiList, "::"));
+    }
+
+    @Test
+    public void synchrnizeTest() {
+        List<Integer> indices = Arrays.asList(1, 2, 3, 4);
+        List<String> content = Arrays.asList("one", "two", "three", "four");
+        List<Boolean> eval = new ArrayList<>();
+        for (List tuple : CollectionUtils.synchronize(indices, content)) {
+            int index = ((int) tuple.get(0));
+            String word = ((String) tuple.get(1));
+            eval.add(word.length() == index);
+        }
+        assertFalse(eval.get(0));
+        assertFalse(eval.get(1));
+        assertFalse(eval.get(2));
+        assertTrue(eval.get(3));
     }
 
 }
