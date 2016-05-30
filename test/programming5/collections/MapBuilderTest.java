234fb55adad8e5aeae2cf2a0c21e1231bdeae2b4
aquirozsea <aquiroz.sea@gmail.com>
2016-05-30 11:38:25 -0400

    Updated MapBuilder tests

--- test/programming5/collections/MapBuilderTest.java
+++ test/programming5/collections/MapBuilderTest.java
@@ -3,8 +3,10 @@ package programming5.collections;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class MapBuilderTest {
 
@@ -14,6 +16,14 @@ public class MapBuilderTest {
         assertEquals(3, MapBuilder.from(1, 2).put(2, 3).put(3, 4).get().size());
 
         assertEquals("value", MapBuilder.in(new HashMap<Integer, String>()).put(1, "value").get().get(1));
+
+        // Should work on any seed map
+        try {
+            MapBuilder.in(new Properties()).put("p1", "v1");
+        }
+        catch (Exception e) {
+            fail("Did not work with Properties class");
+        }
     }
 
 }
